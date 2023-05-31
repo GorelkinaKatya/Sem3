f49408dce9f0ae95159f7ef34f30d0e895004c12
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-31 16:53:19 +0300

    done task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+while (number.Length != 5)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    number = Console.ReadLine()!;
+}
+
+if (number[0] == number[4] || number[1] == number[3])
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число не является палиндромом");
